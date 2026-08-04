e8e9452a5e51ce79a54b9201f5547ec1588a4ca3
ksaweryststsite <badxman2115@gmail.com>
2026-08-04 23:55:20 +0200

    Wydaj 1.0.0-beta.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "pl.ksawery.ktvlauncher"
         minSdk = 30
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.0.0-beta.10"
+        versionCode = 31
+        versionName = "1.0.0-beta.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
