5d6b364ac37e2a5263be567f3c1eb18787a5b120
ksaweryststsite <badxman2115@gmail.com>
2026-08-04 15:03:56 +0200

    Oznacz wydanie jako 1.0.0-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "pl.ksawery.ktvlauncher"
         minSdk = 30
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.7.6-theme3-alignment"
+        versionCode = 20
+        versionName = "1.0.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
