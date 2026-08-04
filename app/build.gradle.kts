1dda5ffc136b69d3561680ba0a90b099ee347762
ksaweryststsite <badxman2115@gmail.com>
2026-08-04 12:43:35 +0200

    Przygotuj wersję 0.7.1 Theme 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "pl.ksawery.ktvlauncher"
         minSdk = 30
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.7.0-theme2"
+        versionCode = 14
+        versionName = "0.7.1-theme2-refined"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
